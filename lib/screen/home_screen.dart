f81048f392bc6a9fd35595a315041f058cc077f7
changhwan77 <ckdghks9741@gmail.com>
2023-04-30 19:52:45 +0900

    웹뷰 다뤄보는 프로젝트

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,43 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class HomeScreen extends StatelessWidget {
+  WebViewController controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setBackgroundColor(const Color(0x00000000))
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onProgress: (int progress) {
+          // Update loading bar.
+        },
+        onPageStarted: (String url) {},
+        onPageFinished: (String url) {},
+        onWebResourceError: (WebResourceError error) {},
+        onNavigationRequest: (NavigationRequest request) {
+          if (request.url.startsWith('https://www.youtube.com/')) {
+            return NavigationDecision.prevent;
+          }
+          return NavigationDecision.navigate;
+        },
+      ),
+    )
+    ..loadRequest(Uri.parse('https://changhwan77.github.io/'));
+
+  HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amberAccent,
+        title: Text('My Blog'),
+        centerTitle: true,
+      ),
+      body: WebViewWidget(
+        controller: controller,
+      ),
+    );
+  }
+}

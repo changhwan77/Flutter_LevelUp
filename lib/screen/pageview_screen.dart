f54ada0f7496744d9010573d4f9ffa6de57ff183
changhwan77 <ckdghks9741@gmail.com>
2023-05-01 13:34:46 +0900

    타이머 기능으로 페이지 자동으로 넘어가게 하는 기능 구현

--- lib/screen/pageview_screen.dart
+++ lib/screen/pageview_screen.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:async';
 
-class PageViewScreen extends StatelessWidget {
+class PageViewScreen extends StatefulWidget {
   const PageViewScreen({super.key});
 
   @override
+  State<PageViewScreen> createState() => _PageViewScreenState();
+}
+
+class _PageViewScreenState extends State<PageViewScreen> {
+  final PageController pageController = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer.periodic(
+      Duration(seconds: 3),
+      (timer) {
+        int? nextPage = pageController.page?.toInt();
+
+        if (nextPage == null) {
+          return;
+        }
+
+        if (nextPage == 4) {
+          nextPage = 0;
+        } else {
+          nextPage++;
+        }
+        pageController.animateToPage(
+          nextPage,
+          duration: Duration(milliseconds: 500),
+          curve: Curves.ease,
+        );
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+
     return Scaffold(
       body: PageView(
+        controller: pageController,
         children: [1, 2, 3, 4, 5]
             .map(
               (number) => Image.asset(

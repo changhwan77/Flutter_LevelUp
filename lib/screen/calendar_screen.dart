ef96b62321c42d6296c533d32951e703f327b5d0
changhwan77 <ckdghks9741@gmail.com>
2023-05-28 01:53:47 +0900

    레이아웃 구현 완료

--- lib/screen/calendar_screen.dart
+++ lib/screen/calendar_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_level_up/component/main_calendar.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:flutter_level_up/component/schedule_card.dart';
+import 'package:flutter_level_up/component/today_banner.dart';
+import 'package:flutter_level_up/component/schedule_bottom_sheet.dart';
+import 'package:flutter_level_up/const/colors.dart';
 
 class CalendarScreen extends StatefulWidget {
   const CalendarScreen({super.key});
@@ -19,12 +23,34 @@ class _CalendarScreenState extends State<CalendarScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: primaryColor,
+        onPressed: () {
+          showModalBottomSheet(
+            isScrollControlled: true,
+            context: context,
+            isDismissible: true,
+            builder: (_) => ScheduleBottomSheet(),
+          );
+        },
+        child: Icon(
+          Icons.add,
+        ),
+      ),
       body: SafeArea(
         child: Column(
           children: [
             MainCalendar(
               selectedDate: selectedDate,
               onDaySelected: onDaySelected,
+            ),
+            TodayBanner(selectedDate: selectedDate, count: 0),
+            SizedBox(
+              height: 8.0,
+            ),
+            ScheduleCard(content: '프로그래밍 공부', endTime: 14, startTime: 12),
+            SizedBox(
+              height: 8.0,
             ),
           ],
         ),
